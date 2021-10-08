3f2d76e83edb9b7410347c000eea457cb4a3b6e8
Vadimochek <56549726+Vadimochek@users.noreply.github.com>
2021-10-08 18:10:16 +0300

    Realize Temperature

--- src/main/java/Help.java
+++ src/main/java/Help.java
@@ -6,6 +6,18 @@ public class Help {
     }
 
     public int getTemperature(double Temp){
-
+        System.out.println("Ваша температура: "+Temp);
+        if (Temp<=36.4){
+            System.out.println("Значение ниже нормы");
+            return -1;
+        }
+        else if (Temp>=36.8){
+            System.out.println("Значение выше нормы");
+            return 1;
+        }
+        else {
+            System.out.println("Нормальное значение");
+            return 0;
+        }
     }
 }
